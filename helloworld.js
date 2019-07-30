749b1ef76476479da64a88b73b4dfadb24753ba6
jmacioci <49567990+jmacioci@users.noreply.github.com>
2019-07-30 11:04:35 -0400

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
-  res.end('Hello John, this is your first Jenkins Job');
+  res.end('Hello John, this is your first github trigger');
 });
 server.listen(8080);

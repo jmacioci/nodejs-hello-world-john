0c2f29ef9e69a2a9fad0ab11312736edd3f8e334
jmacioci <49567990+jmacioci@users.noreply.github.com>
2019-07-31 12:42:46 -0400

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
-  res.end('Hello John, this is your 1st github trigger');
+  res.end('Hello John, this is your 2nd github trigger');
 });
 server.listen(8080);
